d331b637dd2e4be1e2fb6b45a3c34b8c5f11812a
OlivMagno <olivmagno1@gmail.com>
2022-07-06 17:14:45 -0300

    set menu structure

--- src/components/menu/MenuNav.vue
+++ src/components/menu/MenuNav.vue
@@ -1,10 +1,18 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Home</router-link>
     <router-link to="/about">About</router-link>
   </nav>
 </template>
 
 <script></script>
 
-<style scoped></style>
+<style scoped>
+nav {
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+
+  width: 25vw;
+}
+</style>
--- src/components/menu/MenuStructure.vue
+++ src/components/menu/MenuStructure.vue
@@ -1,7 +1,9 @@
 <template>
-  <h1>LOGO</h1>
-  <MenuNav />
-  <h1>DOWNLOADBUTTON</h1>
+  <div class="container">
+    <h1>LOGO</h1>
+    <MenuNav />
+    <h1>BUTTON</h1>
+  </div>
 </template>
 
 <script>
@@ -14,3 +16,14 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-between;
+  align-items: center;
+  margin-left: 5vw;
+  margin-right: 5vw;
+}
+</style>
