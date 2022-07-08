74c44eb8cd69c3fcc07d1aadc69aafe74b331c57
OlivMagno <olivmagno1@gmail.com>
2022-07-08 09:03:47 -0300

    fixed placeholder views

--- src/views/ExamplesView.vue
+++ src/views/ExamplesView.vue
@@ -1,5 +1,7 @@
 <template>
-  <h1>Examples</h1>
+  <div class="container">
+    <h1>Examples</h1>
+  </div>
 </template>
 
 <script>
@@ -7,3 +9,17 @@ export default {
   name: "ExamplesView",
 };
 </script>
+
+<style scoped>
+.container {
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: space-evenly;
+  align-items: center;
+
+  width: 90vw;
+  margin-top: 12vh;
+  margin-left: 5vw;
+  margin-right: 5vw;
+}
+</style>
